924b018a40700d4c91c241519846e178a8a02461
Shilpa S M <Shilpa S M@LAPTOP-1P02B106>
2023-02-20 12:08:19 +0530

    first commit

---
+++ SpringIOC/src/main/java/org/tnif/springIOC/Jio.java
@@ -0,0 +1,16 @@
+package org.tnif.springIOC;
+
+//to demonstrate a program on spring IOC
+//implementable class for Sim Interface
+public class Jio implements Sim{
+
+	public void call() {
+		System.out.println("I am using jio calling");
+		
+	}
+
+	public void data() {
+		System.out.println("I am using jio data");		
+	}
+
+}
