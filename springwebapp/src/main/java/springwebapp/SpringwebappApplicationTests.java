c8ff9ccebda33ad5b7585fb57720a5b8cc8ad015
Shilpa S M <Shilpa S M@LAPTOP-1P02B106>
2023-02-21 09:06:20 +0530

    first commit

---
+++ springwebapp/src/main/java/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
